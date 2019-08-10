db35fba4d1e3364141d3d48034f47b440d23c2ce
Julio Lemus <julio.lemus.lara@gmail.com>
2019-08-10 11:46:34 -0500

    project created with shared library, android and ios projects

---
+++ Weather/ios/Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Julio Lemus on 8/10/19.
+//  Copyright © 2019 Julio Lemus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
